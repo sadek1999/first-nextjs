3924886d3ffaf5eccd781deac029865964964237
Sadek Ali <sadekali1530@gmail.com>
2024-01-05 23:58:12 +0600

    set error pagee

---
+++ src/app/error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Errorpages = () => {
+    return (
+        <div>
+            <h1>Sum think going roerg</h1>
+        </div>
+    );
+};
+
+export default Errorpages;
---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+
+
+const LoadingPage = () => {
+    return (
+        <div>
+          <h1 className='text-5xl'>Loading page !!!!!!!!!!!!!!!!</h1>  
+        </div>
+    );
+};
+
+export default LoadingPage;
