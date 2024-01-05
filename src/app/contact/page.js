9736133b8d79600fc8f61c7d4c76577eb2fa7118
Sadek Ali <sadekali1530@gmail.com>
2024-01-05 23:36:42 +0600

    set clind component in next js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,13 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 
 const contactPge = () => {
+    let [count ,setcount]=useState(0)
     return (
         <div>
         <h1>This is contact page ll........................</h1>
+        <h1>Count value = {count}</h1>
+        <button className='btn btn-primary' onClick={()=>{setcount(count++)}}>incress</button>
         </div>
     );
 };
