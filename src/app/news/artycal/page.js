a2ad768b3cc4b156c09bad2b52dd6a27c87a3edb
Sadek Ali <sadekali1530@gmail.com>
2024-01-05 23:21:01 +0600

    set nested  page

---
+++ src/app/news/artycal/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Artycals = () => {
+    return (
+        <div>
+            <h1>This is an artycals page .........</h1>
+            
+        </div>
+    );
+};
+
+export default Artycals;
---
+++ src/app/news/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Newspage = () => {
+    return (
+        <div>
+         <h1 className='text-center'>This is a news page .......</h1>   
+        </div>
+    );
+};
+
+export default Newspage;
