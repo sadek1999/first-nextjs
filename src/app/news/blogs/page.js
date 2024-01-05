95986b28d54b3af89fd943d1f56efe5344aed3d1
Sadek Ali <sadekali1530@gmail.com>
2024-01-05 23:14:36 +0600

    set nastaded dynamic page

---
+++ src/app/news/blogs/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogpage = () => {
+    return (
+        <div>
+          <h1 className='text-red-300'>wel come the new blog page ......</h1>  
+        </div>
+    );
+};
+
+export default Blogpage;
--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Dynamicpage = ({params}) => {
-    console.log(params)
+const Dynamicpage = ({params ,searchParams}) => {
+    console.log(params ,searchParams)
     return (
         <div>
             <h1>This is dynamic page ........</h1>
