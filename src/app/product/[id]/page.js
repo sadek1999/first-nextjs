9d173ed27d73cfca5988f7c66ce79487fdbb3b6f
Sadek Ali <sadekali1530@gmail.com>
2024-01-05 23:02:30 +0600

    set dynamic page

---
+++ src/app/product/[id]/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dynamicpage = ({params}) => {
+    console.log(params)
+    return (
+        <div>
+            <h1>This is dynamic page ........</h1>
+        </div>
+    );
+};
+
+export default Dynamicpage;
