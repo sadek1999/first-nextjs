dd057bb02f4ffa602d8e564fbead2171fa747b66
Sadek Ali <sadekali1530@gmail.com>
2024-01-07 20:53:53 +0600

    set group layout

---
+++ src/app/(withlayout)/alli/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Alipage = () => {
+    return (
+        <div>
+            <h1 className='text-4xl'>This is my page</h1>
+        </div>
+    );
+};
+
+export default Alipage;
