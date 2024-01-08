7d6d30cf9fdaa9dfccf00698a11e61881e404a3c
Sadek Ali <sadekali1530@gmail.com>
2024-01-08 19:02:24 +0600

    set imgae component in my app

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 export const metadata={
@@ -9,6 +10,7 @@ const Aboutpage = () => {
     return (
         <div>
             <h1>Welcome to about page ............</h1>
+            <Image src="https://faraitltd.com/wp-content/uploads/2023/02/Screenshot_7.jpg" alt='web img' width={200} height={400}/>
             
         </div>
     );
