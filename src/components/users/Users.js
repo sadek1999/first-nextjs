4712322a514266a7a024a250407897f1cc6eaa09
Sadek Ali <sadekali1530@gmail.com>
2024-01-10 21:45:11 +0600

    set data

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -5,16 +5,29 @@ import { useEffect, useState } from "react";
 
 
 const Users = () => {
-    const [users,setusers]=useState([])
-    
-    useEffect( ()=>{
+    const [users, setusers] = useState([])
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res=> res.json())
-        .then(data=>{console.log(data)})
-    },[])
+            .then(res => res.json())
+            .then(data => { console.log(data) })
+    }, [])
     return (
         <div>
-            <p>This is user component</p>
+            <h1>Total users: </h1>
+
+            {
+                data.map(d => {<div className="card w-96 bg-base-100 shadow-xl">
+                        <div className="card-body">
+                            <h2 className="card-title">{data.name}</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                })
+            }
         </div>
     );
 };
